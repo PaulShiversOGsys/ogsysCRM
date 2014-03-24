3b401dd89afad4a9240019daa83fc3d2c0f1a0da
Justin <justinpatterson@gmail.com>
2014-03-24 17:54:12 -0500

    Added Untracked file

---
+++ ogsysCRM/ViewModels/DetailsCustomerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ogsysCRM.Models;
+
+namespace ogsysCRM.ViewModels
+{
+    public class DetailsCustomerViewModel : CustomerViewModel
+    {
+        public int Id { get; set; }
+        public ICollection<NoteViewModel> Notes { get; set; }
+
+        [Display(Name = "Address")]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+
+        [Display(Name = "")]
+        [DataType(DataType.ImageUrl)]
+        public string AvatarUrl { get; set; }
+
+    }
+}
